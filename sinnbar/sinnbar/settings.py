7138e8b75f38b638c85d2d86cc10462cdff30df4
hendrik <hendrik@sinnbar.de>
2021-10-21 22:07:26 +0200

    fix: .env settings.py

--- sinnbar/sinnbar/settings.py
+++ sinnbar/sinnbar/settings.py
@@ -14,11 +14,15 @@ import os
 from pathlib import Path
 from datetime import timedelta
 import django_heroku
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+environ.Env.read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-_avp2)gg)^4j-r40tshh^it$ty(7z_(rs$^qb*m4v7v^_qgx#e'
